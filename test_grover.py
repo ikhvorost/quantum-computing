b8a48bcc59fd251073e2bbbdd9592121802e9ce8
Iurii Khvorost <iurii.khvorost@gmail.com>
2019-10-08 22:24:35 +0300

    Updated tests

--- test_grover.py
+++ test_grover.py
@@ -5,10 +5,10 @@ token = os.environ.get('IBMQ_API_TOKEN')
 
 def test_aer_simulator():
     oracle = 3
-    result = grover.search(4, oracle, "Aer", "qasm_simulator", None, 100, False)
+    result = grover.search(4, oracle, "Aer", "qasm_simulator", None, 100, False, None)
     assert result == oracle
 
 def test_ibmq_simulator():
     oracle = 3
-    result = grover.search(4, oracle, "IBMQ", "ibmq_qasm_simulator", token, 100, False)
+    result = grover.search(4, oracle, "IBMQ", "ibmq_qasm_simulator", token, 100, False, None)
     assert result == oracle
